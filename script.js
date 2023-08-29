cb2d117324f069ebc60a7031e2fb8f1ba068f4de
sAndreas19 <hasansihombing0709@gmail.com>
2023-08-29 16:33:37 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const message = document.getElementById("message").value;
+    
+    // Mengganti "youremail@example.com" dengan alamat email Anda
+    const mailtoLink = `mailto:youremail@example.com?subject=Pesan dari ${name}&body=${encodeURIComponent(message)}`;
+    
+    window.location.href = mailtoLink;
+});
