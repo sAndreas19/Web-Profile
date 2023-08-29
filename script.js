d56ba8bea237676682335ac96e0d4a97c9d77da1
sAndreas19 <hasansihombing0709@gmail.com>
2023-08-29 22:41:34 +0700

    update responsive

--- script.js
+++ script.js
@@ -5,7 +5,6 @@ document.getElementById("contactForm").addEventListener("submit", function(event
     const email = document.getElementById("email").value;
     const message = document.getElementById("message").value;
     
-    // Mengganti "youremail@example.com" dengan alamat email Anda
     const mailtoLink = `mailto:hasansihombing0709@gmailcom?subject=Pesan dari ${name}&body=${encodeURIComponent(message)}`;
     
     window.location.href = mailtoLink;
